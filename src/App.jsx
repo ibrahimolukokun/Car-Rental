6b393aecdc5eb31fd3d6cf0db170911472e2ce8e
“Ibrahim <iolukokun@gmail.com>
2025-07-01 15:07:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center mt-10">
+        Welcome to the Car Rental App
+      </h1>
+      <p className="text-center mt-4">
+        This is a simple application built with React and Tailwind CSS.
+      </p>
+
+    </div>
+  )
+}
+
+export default App
